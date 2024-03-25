4bb175c717e586cd77f81383bb5d5f8b837677df
Francisco Nunes Pinheiro <124183034+chicovm2@users.noreply.github.com>
2024-03-25 20:59:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const html = document.querySelector('html');
+const botaoFoco = document.querySelector('.app__card-button--foco');
+const botaoCurto = document.querySelector('.app__card-button--curto');
+const botaoLongo = document.querySelector('.app__card-button--longo');
+
+botaoFoco.
